c7541cd090b309e49c65eb624efc15a727c382d1
AllyintheCode <allyasadzade@gmail.com>
2025-03-24 05:19:02 +0400

    hoem task

---
+++ app.js
@@ -0,0 +1,82 @@
+// function salam() {
+//   let a = [1, 2, 3]
+//   let b = [4, 5, 6]
+//   return a.concat(b)
+// }
+// console.log(salam);
+
+// *function sagol() {
+//   let a = [1, 2, 3, 4, 5, 6]
+//   let b = [1, 2, 3]
+//   return a.concat(b)
+// }
+// console.log(sagol());
+
+// function sortArray() {
+//   let array = [
+//     1, 6, 34, 5, 1, -4, 54, 76, 23, 65, 3, 50, 45, 100, 2346, -52, 6545, 0, 45, 22,
+//   ];
+//   let a = array.sort((a, b) => { return a - b })
+//   return a
+// }
+// console.log(sortArray());
+
+//* function menfiCixaran() {
+//   let a = [1, 5, -47, 9, 54, 4, 6, 9, 85, 30]
+//   for (let i of a) {
+//     if (i < 0) {
+//       console.log([i]);
+
+
+
+//     }
+//   }
+
+// }
+
+// console.log(menfiCixaran());
+
+// function kebabToUpper(metn) {
+//   let a = metn.split("-").join("_").toUpperCase()
+//   return a
+// }
+// console.log(kebabToUpper("Kebab-case"));
+
+
+// let a = [1, 5, -7, 3, -9, 4, -6, 2];
+
+// for (let i of a) {
+//   if (i > 0) {
+//     let c = i * 2
+//     console.log(c);
+//   } else if (i < 0) {
+//     let b = i * -1
+//     console.log(b);
+
+//   }
+
+// }
+
+// *function alariTapan() {
+//   let namesArray = [
+//     "Alice",
+//     "Bob",
+//     "Catherine",
+//     "David",
+//     "Eva",
+//     "Andrew",
+//     "Frank",
+//     "Anna",
+//     "George",
+//     "Alex",
+//   ];
+//   let frist = namesArray.find(myFunction)
+//   for (let i of namesArray) { }
+//   function myFunction(value, index, array) {
+//     return i[0] === "A"
+//   }
+//   console.log(frist);
+
+// }
+
+// console.log(alariTapan());
